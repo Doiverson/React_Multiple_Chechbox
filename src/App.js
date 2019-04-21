d800916c67e64102062bdf9f5beab758bb7f7b10
Shosuke Doi <doiverson.canada@gmail.com>
2019-04-20 17:38:38 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import  CheckBox  from './CheckBox'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fruites: [
+        {id: 1, value: "banana", isChecked: false},
+        {id: 2, value: "apple", isChecked: false},
+        {id: 3, value: "mango", isChecked: false},
+        {id: 4, value: "grap", isChecked: false}
+      ]
+    }
+  }
+
+  handleAllChecked = (event) => {
+    let fruites = this.state.fruites
+    fruites.forEach(fruite => fruite.isChecked = event.target.checked)
+    this.setState({fruites: fruites})
+  }
+
+  handleCheckChildElement = (event) => {
+    let fruites = this.state.fruites
+    fruites.forEach(fruite => {
+      if (fruite.value === event.target.value)
+        fruite.isChecked =  event.target.checked
+    })
+    this.setState({fruites: fruites})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <h1> Check and Uncheck All Example </h1>
+          <input type="checkbox" onClick={this.handleAllChecked}  value="checkedall" /> Check / Uncheck All
+          <ul>
+            {
+              this.state.fruites.map((fruite) => {
+                return (<CheckBox key={fruite.id} handleCheckChildElement={this.handleCheckChildElement}  {...fruite} />)
+              })
+            }
+          </ul>
+        </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/CheckBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const CheckBox = props => {
+
+    console.log(props);
+
+    return (
+        <li>
+            <input key={props.id} onChange={props.handleCheckChildElement} type="checkbox" checked={props.isChecked} value={props.value} /> {props.value}
+        </li>
+    )
+}
+
+export default CheckBox
